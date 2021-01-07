d1e57d4ddd781779dbe45a1ebadc486833b9c196
Lenard92 <lenardvwinssen@hotmail.com>
2021-01-07 10:35:01 +0100

    app login

---
+++ GripOpGras/GripOpGras/Views/LoginPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using GripOpGras.Models;
+
+
+namespace GripOpGras.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+        
+
+        public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if (user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login Succes", "Oke");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login not correct", "Oke");
+            }
+            }
+        }
+    }
+
+
